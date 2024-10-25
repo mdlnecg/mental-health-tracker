f90a2ecad702c826ea48c7f080a3363596cd0d95
mdlnecg <mdlnecg@gmail.com>
2024-10-26 02:16:45 +0700

    Updates for Tutorial 5: Maaf, baru sadar belum dipush T__T

--- main/views.py
+++ main/views.py
@@ -9,18 +9,19 @@ from main.models import MoodEntry
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 # Login required
 @login_required(login_url='/login')
 
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
 
     context = {
         'name': request.user.username,
         'class': 'PBP D',
         'npm': '2306123456',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -39,10 +40,7 @@ def create_mood_entry(request):
     return render(request, "create_mood_entry.html", context)
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
-
-def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
@@ -81,6 +79,7 @@ def login_user(request):
         return response
 
    else:
+      messages.error(request, "Invalid username or password. Please try again.")
       form = AuthenticationForm(request)
    context = {'form': form}
    return render(request, 'login.html', context)
@@ -107,3 +106,20 @@ def delete_mood(request, id):
     mood = MoodEntry.objects.get(pk = id)
     mood.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) # strip HTML tags!
+    feelings = strip_tags(request.POST.get("feelings")) # strip HTML tags!
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
